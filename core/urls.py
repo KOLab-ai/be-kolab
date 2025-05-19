3ffdf3f4d05b5f82fc104ebf619a0baae4c2ae64
Faqih Suyudi <faqihsuyudi2020@gmail.com>
2025-05-19 22:01:16 +0800

    google auth

---
+++ core/urls.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from django.urls import include, path, re_path
+
+from core.views import GoogleLogin
+
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("api/v1/auth/", include("dj_rest_auth.urls")),
+    re_path(r"^api/v1/auth/accounts/", include("allauth.urls")),
+    path("api/v1/auth/registration/", include("dj_rest_auth.registration.urls")),
+    path("api/v1/auth/google/", GoogleLogin.as_view(), name="google_login"),
+]
---
+++ core/views.py
@@ -0,0 +1,39 @@
+from allauth.socialaccount.providers.google.views import GoogleOAuth2Adapter
+from allauth.socialaccount.providers.oauth2.client import OAuth2Client
+from dj_rest_auth.registration.views import SocialLoginView
+from django.conf import settings
+
+
+class CustomGoogleOAuth2Client(OAuth2Client):
+    """
+    Workaround bug di dj-rest-auth: https://github.com/iMerica/dj-rest-auth/issues/673
+    """
+    def __init__(
+        self,
+        request,
+        consumer_key,
+        consumer_secret,
+        access_token_method,
+        access_token_url,
+        callback_url,
+        _scope,  # This is fix for incompatibility between django-allauth==65.3.1 and dj-rest-auth==7.0.1
+        scope_delimiter=" ",
+        headers=None,
+        basic_auth=False,
+    ):
+        super().__init__(
+            request,
+            consumer_key,
+            consumer_secret,
+            access_token_method,
+            access_token_url,
+            callback_url,
+            scope_delimiter,
+            headers,
+            basic_auth,
+        )
+
+class GoogleLogin(SocialLoginView):
+    adapter_class = GoogleOAuth2Adapter
+    callback_url = settings.GOOGLE_CALLBACK_URL
+    client_class = CustomGoogleOAuth2Client
