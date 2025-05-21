a71ac05cc375b1a6cad16fc6255e7a90b66e9f4d
Faqih Suyudi <faqihsuyudi2020@gmail.com>
2025-05-22 00:24:34 +0800

    chore: register influencers to admin panel

--- influencers/admin.py
+++ influencers/admin.py
@@ -1,3 +1,39 @@
 from django.contrib import admin
+from .models import Category, Domicile, Influencer, SocialPlatform
 
-# Register your models here.
+
+@admin.register(Influencer)
+class InfluencerAdmin(admin.ModelAdmin):
+    list_display = [
+        "full_name",
+        "domicile",
+        "description",
+        "email",
+        "phone",
+        "address",
+        # "categories",
+    ]
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ["name"]
+
+
+@admin.register(Domicile)
+class DomicileAdmin(admin.ModelAdmin):
+    list_display = ["city"]
+
+
+@admin.register(SocialPlatform)
+class SocialPlatformAdmin(admin.ModelAdmin):
+    list_display = [
+        "platform",
+        "profile_url",
+        "username",
+        "followers",
+        "engagement_rate",
+        "avg_likes_per_post",
+        "avg_comments_per_post",
+        "avg_views_per_post",
+    ]
