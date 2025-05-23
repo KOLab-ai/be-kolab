d6832b9f075d9a50b367356c74d4b8592868b253
Faqih Suyudi <faqihsuyudi2020@gmail.com>
2025-05-23 08:04:49 +0800

    feat: category and domicile endpoint

--- influencers/urls.py
+++ influencers/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import InfluencerViewSet
+from .views import CategoryViewSet, DomicileViewSet, InfluencerViewSet
 
 router = DefaultRouter()
 router.register(r'influencers', InfluencerViewSet, basename='influencer')
+router.register(r'categories', CategoryViewSet, basename='categories')
+router.register(r'domiciles', DomicileViewSet, basename='domiciles')
 
 urlpatterns = [
     path('', include(router.urls)),
--- influencers/views.py
+++ influencers/views.py
@@ -1,9 +1,22 @@
 from rest_framework import viewsets
+from rest_framework.pagination import BasePagination
 from rest_framework.permissions import IsAuthenticated
-from .models import Influencer
-from .serializers import InfluencerSerializer
+from .models import Category, Domicile, Influencer
+from .serializers import CategorySerializer, DomicileSerializer, InfluencerSerializer
 
 class InfluencerViewSet(viewsets.ModelViewSet):
     queryset = Influencer.objects.all()
     serializer_class = InfluencerSerializer
     permission_classes = [IsAuthenticated]
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    permission_classes = [IsAuthenticated]
+    pagination_class = None
+
+class DomicileViewSet(viewsets.ModelViewSet):
+    queryset = Domicile.objects.all()
+    serializer_class = DomicileSerializer
+    permission_classes = [IsAuthenticated]
+    pagination_class = None
